1eb8ec430121a380287d8fc7ca5aa1055b430131
Paula Gearon <pgearon@imohealth.com>
2023-05-24 22:47:41 -0400

    update to 0.1.5

--- build.clj
+++ build.clj
@@ -4,13 +4,13 @@
             [org.corfield.build :as bb]))
 
 (def lib 'org.clojars.quoll/raphael)
-(def version "0.1.3")
-#_ ; alternatively, use MAJOR.MINOR.COMMITS:
-(def version (format "1.0.%s" (b/git-count-revs nil)))
+(def version "0.1.5")
 
+;; clojure -T:build test
 (defn test "Run the tests." [opts]
   (bb/run-tests opts))
 
+;; clojure -T:build ci
 (defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
   (-> opts
       (assoc :lib lib :version version)
@@ -18,11 +18,13 @@
       (bb/clean)
       (bb/jar)))
 
+;; clojure -T:build install
 (defn install "Install the JAR locally." [opts]
   (-> opts
       (assoc :lib lib :version version)
       (bb/install)))
 
+;; clojure -T:build deploy
 (defn deploy "Deploy the JAR to Clojars." [opts]
   (-> opts
       (assoc :lib lib :version version)
